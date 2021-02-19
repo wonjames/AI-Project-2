b6f0b368ba111963bd051d1eaacbf94089e11045
wonjames <wonjames99@gwmail.gwu.edu>
2021-02-18 22:13:43 -0800

    adding reccursion

--- project-2.py
+++ project-2.py
@@ -25,29 +25,28 @@ def isColorable(G, max_color):
     node_info = build_dict(colors, key="node")
     #print(node_info)
     graph_list = list(G.neighbors(first_node))
-    next_node = getNextNode(G, graph_list, first_node, node_info)
-    setNodeColor(next_node, node_info)
+    getNextNode(G, graph_list, first_node, node_info, 0)
     #print("next node: ", next_node)
 
-    graph_list = list(G.neighbors(next_node))
-    print(graph_list)
-    next_node = getNextNode(G,graph_list, next_node, node_info)
-    setNodeColor(next_node, node_info)
+    #graph_list = list(G.neighbors(next_node))
+    #print(graph_list)
+    #next_node = getNextNode(G,graph_list, next_node, node_info)
+    #setNodeColor(next_node, node_info)
     #print("next node: ", next_node)
     
-    graph_list = list(G.neighbors(next_node))
-    print(graph_list)
-    next_node = getNextNode(G,graph_list, next_node, node_info)
-    setNodeColor(next_node, node_info)
+    #graph_list = list(G.neighbors(next_node))
+    #print(graph_list)
+    #next_node = getNextNode(G,graph_list, next_node, node_info)
+    #setNodeColor(next_node, node_info)
     #print("next node: ", next_node)
     
-    graph_list = list(G.neighbors(next_node))
-    print(graph_list)
-    next_node = getNextNode(G,graph_list, next_node, node_info)
-    setNodeColor(next_node, node_info)
+    #graph_list = list(G.neighbors(next_node))
+    #print(graph_list)
+    #next_node = getNextNode(G,graph_list, next_node, node_info)
+    #setNodeColor(next_node, node_info)
     #print("next node: ", next_node)
     
-    print(node_info)
+    print("Final: ", node_info)
     #next_node = node_info.get(next_node)
     #setNodeColor(next_node)
     #print(next_node)
@@ -64,18 +63,19 @@ def isColorable(G, max_color):
     #print(max(a))
 
 def setNodeColor(node, node_info):
-    
     node = node_info.get(node)
-    
     if type(node['colors']) != int:
         color = node['colors'][0]
         node['colors'] = color
-    print("node info: ", node_info)
+    #print("node info: ", node_info)
 
-def getNextNode(G, neighbors, node, node_info):
+def getNextNode(G, neighbors, node, node_info, v):
+    if v == len(G.nodes):
+        return True
     # neighbors of A --> B, C
     mrv = 999
     next_node = None
+    arr = []
     for n in neighbors:
         n_dict = node_info.get(n)
         n_dict['colors'] = removeColor(node_info.get(node), n_dict)
@@ -86,10 +86,42 @@ def getNextNode(G, neighbors, node, node_info):
         if mrv > n_color_length:
             mrv = n_color_length
             next_node = n
-    return next_node
+            mrv_dict = {"node": next_node, "mrv": mrv}
+            arr.append(mrv_dict)
+        else:
+            mrv_dict = {"node": n, "mrv": n_color_length}
+            arr.append(mrv_dict)
+    for i in range(len(arr)):
+        min_mrv = 999
+        node = arr[0]
+        var = 0
+        for iter, x in enumerate(arr):
+            mrv = x['mrv']
+            if mrv < min_mrv:
+                min_mrv = mrv
+                node = x
+                var = iter
+        if min_mrv == 999:
+            return False
+        if len(arr):
+            arr.pop(var)
+        
+        graph_list = list(G.neighbors(node['node']))
+        setNodeColor(node['node'], node_info)
+        print("Next Node:", i, node)
+        print('Nodes neighbors: ', graph_list)
+        if getNextNode(G,graph_list, node['node'], node_info, v+1) == True:
+            return True
+
+
+    return False
+    #graph_list = list(G.neighbors(next_node))
+    #setNodeColor(next_node, node_info)
+    #next_node = getNextNode(G,graph_list, next_node, node_info, v+1)
+    
 
 def removeColor(n, n2):
-    print(n['colors'])
+    #print(n['colors'])
     n_color = n['colors']
     n2_color = n2['colors']
     if type(n2_color) == int:
@@ -105,7 +137,9 @@ if __name__ == '__main__':
     G.add_edge('A', 'B')
     G.add_edge('B', 'D')
     G.add_edge('A', 'C')
-    G.add_edge('C', 'D')
-    G.add_edge('B', 'E')
+    G.add_edge('A', 'D')
+    G.add_edge('C', 'E')
+    G.add_edge('D', 'E')
+    G.add_edge('B', 'F')
     isColorable(G, 3)
     
